b635ab5398cf80a94ca55ca30453d244bff6f99d
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-16 14:56:38 +0100

    Creazione Layout principale

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,25 @@
+//! Importazione dipendenze
+import { Outlet } from "react-router-dom"
+
+//! Importazione componenti
+import Navbar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
+//! Creazione Layout
 export default function Layout () {
     return (
         <>
+            <header>
+                <nav>
+                    <Navbar />
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </>
     )
 }
